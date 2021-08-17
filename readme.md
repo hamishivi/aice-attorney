40975b0fe120f11ad1252a09a304950e9008ff20
hamishivi <hamishivi@gmail.com>
2021-08-17 22:18:09 +1000

    update links and readme

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # AI-ce Attorney
 
-Bot that generates ace attorney scripts and converts them into video using objection engine. Nothing fancy, just playing around with GPT-x type models.
+[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/hamishivi/aice-attorney)
+
+Bot that generates ace attorney scripts and converts them into video using objection engine. Nothing fancy, just playing around with GPT-type models.
 
 Objection engine from [here](https://github.com/LuisMayo/objection_engine), but lightly altered to fit my own code (hence it is not installed as a submodule here).
--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,11 @@ def gen_and_display():
         os.mkdir('smaller_trained_model')
     if not os.path.exists("smaller_trained_model/config.json"):
         gdown.download(
-            "https://drive.google.com/uc?id=1JkPZqaAuoX7eh4IuDQMVYpyu2IciQ-73", "smaller_trained_model/config.json", quiet=False
+            "https://drive.google.com/uc?id=1K6SIwi8HC5seAWQryGhg4lpubPdm4oFl", "smaller_trained_model/config.json", quiet=False
         )
     if not os.path.exists("smaller_trained_model/pytorch_model.bin"):
         gdown.download(
-            "https://drive.google.com/uc?id=1OfAAfPNxorF-_hy7k1fysLBc6JJc7X41", "smaller_trained_model/pytorch_model.bin", quiet=False
+            "https://drive.google.com/uc?id=1yniydqMqHFi1khDMwuQlMxvxgQThGbt6", "smaller_trained_model/pytorch_model.bin", quiet=False
         )
     # generate file
     random_gen('thing.mp4')
